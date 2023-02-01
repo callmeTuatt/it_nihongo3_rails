8e16eb224cf4ba013a65bff7d05888cd25aabd99
callmeTuatt <trananh16112001@gmail.com>
2023-02-01 04:36:30 +0000

    Add name age to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_01_035438) do
+ActiveRecord::Schema.define(version: 2023_02_01_041815) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
